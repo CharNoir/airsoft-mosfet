b90349b7672c4b07d9cd952597304087998fedf0
CharNoir <dobbiharrypotter@yandex.ru>
2023-03-27 19:36:00 +0200

    Create test.ino

---
+++ test/test.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+/*
+Modes
+  1) SAFETY
+  2)
+    2.1) SINGLE CLICK
+    2.2) DOUBLE CLICK
+  3) 
+    3.1) AUTO
+    3.2) BURST
+  
+  Mode switches with a potentiometr.
+  ?SubMode switches with a button.?
+
+If button is pressed in N seconds after connecting the battery - 11.1V, else 7.4V.
+  After connecting battery yellow light, If button pressed blink red light. Turn off LED after a setup.
+If voltage is below minimum - you can not fire, indicate with LED.
+
+
+*/
